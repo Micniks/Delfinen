cbbd057aa90faf34d6aa71c4ea71fc1d3135bc3e
Michael N. Korsgaard <Bruger@10.50.131.70>
2019-05-08 16:01:36 +0200

    spelling correction

--- Delfinen/test/testbusinesslogic/CalculateInterrestTest.java
+++ Delfinen/test/testbusinesslogic/CalculateInterrestTest.java
@@ -5,16 +5,27 @@
  */
 package testbusinesslogic;
 
+import businesslogic.Controller;
+import businesslogic.Member;
+import datasource.FakeFacade;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import presentation.FakeUI;
 
 /**
  *
  * @author Michael N. Korsgaard
  */
 public class CalculateInterrestTest {
-    
-    public CalculateInterrestTest() {
+
+    @Test
+    public void CalculateInactiveMembertest() {
+        String[] input = {};
+        FakeUI ui = new FakeUI(input);
+        FakeFacade db = new FakeFacade();
+        Controller ctrl = new Controller(ui, db);
+        Member member = new Member(1, "Jens", 22, false, false, 0, "08-05-2019", "08-05-2019"); 
+        
     }
-    
+
 }
